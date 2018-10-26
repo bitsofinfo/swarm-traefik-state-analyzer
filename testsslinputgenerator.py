7248430e8da3684ba7316eb7d312b4ffe80cce08
bitsofinfo <bitsofinfo.g@gmail.com>
2018-10-25 19:01:51 -0600

    collapse on fqdn re match

--- testsslinputgenerator.py
+++ testsslinputgenerator.py
@@ -27,7 +27,7 @@ def dedup(list_of_objects):
 
 
 # Does the bulk of the work
-def execute(input_filename,output_filename,stdout_result,fqdn_filter,testssl_nonfile_args,testssl_outputdir):
+def execute(input_filename,output_filename,stdout_result,fqdn_filter,testssl_nonfile_args,testssl_outputdir,uri_bucket_filter,collapse_on_fqdn_filter):
 
 
     try:
@@ -41,42 +41,76 @@ def execute(input_filename,output_filename,stdout_result,fqdn_filter,testssl_non
 
         testssl_sh_commands = ""
 
+        # fqdn filter
         fqdn_re_filter = None
         if fqdn_filter:
             fqdn_re_filter = re.compile(fqdn_filter,re.M|re.I)
+
+        # uri bucket filter
+        uri_bucket_re_filter = None
+        if uri_bucket_filter:
+            uri_bucket_re_filter = re.compile(uri_bucket_filter,re.M|re.I)
+
+        # collapse on fqdn filter
+        collapse_on_fqdn_re_filter = None
+        collapsed_fqdns_found = []
+        if collapse_on_fqdn_filter:
+            collapse_on_fqdn_re_filter = re.compile(collapse_on_fqdn_filter,re.M|re.I)
 
         # process it all
         for service_record in layer_check_db:
 
             # no replicas? skip
             if service_record['replicas'] == 0:
+                logging.debug("Skipping, no replicas for: " + service_record['name'])
                 continue
 
             # we have replicas and lets do some checks
             for via_bucket in service_record['unique_entrypoint_uris']:
+
+                # skip buckets we don't want
+                if uri_bucket_re_filter:
+                    if not uri_bucket_re_filter.match(via_bucket):
+                        logging.debug("Skipping " + service_record['name'] + " unique_entrypoint_uris." + via_bucket)
+                        continue
+
+                # bucket is ok proceed
                 for target_url in service_record['unique_entrypoint_uris'][via_bucket]:
+
                     if 'https' in target_url:
-                        url_acceptable = True
+
+                        # matches our fqdn filter if present?
                         if fqdn_re_filter:
                             if not fqdn_re_filter.match(target_url):
-                                url_acceptable = False
+                                logging.debug("Skipping fqdn_filter no match: " + service_record['name'] + " -> " + target_url)
+                                continue
 
-                        # only proceed if we can based on acceptability
-                        if url_acceptable:
-                            # create a unique name for each filename, containing relevant swarm info
-                            filename = service_record["swarm_name"] + "__" +service_record["name"] + "__" + target_url.replace("https://","").replace(":","_")
+                        # matches our fqdn filter if present?
+                        if collapse_on_fqdn_re_filter:
+                            collapse_fqdn_match = collapse_on_fqdn_re_filter.search(target_url)
+                            if collapse_fqdn_match:
+                                collapsed_fqdn_found = collapse_fqdn_match.group(1)
+                                if collapsed_fqdn_found in collapsed_fqdns_found:
+                                    logging.debug("Skipping " + service_record['name'] + " url " + target_url + " as collapse_on_fqdn_filter already has a match for: " + collapsed_fqdn_found)
+                                    continue
+                                elif collapsed_fqdn_found:
+                                    logging.debug("collapse_on_fqdn_filter matched " + service_record['name'] + " url " + target_url + " will use this as our collapsed match for fqdn: " + collapsed_fqdn_found)
+                                    collapsed_fqdns_found.append(collapsed_fqdn_found)
 
-                            # specify a directory path to hold all testssl arg: --*file <filenames>
-                            file_arg_target_dir = testssl_outputdir + "/" + service_record["swarm_name"] + "/" +service_record["name"] + "/" + target_url.replace("https://","").replace(":","_")
+                        # create a unique name for each filename, containing relevant swarm info
+                        filename = service_record["swarm_name"] + "__" +service_record["name"] + "__" + target_url.replace("https://","").replace(":","_")
 
-                            # filenames for all types
-                            logfilename = file_arg_target_dir+"/testssl__"+filename+".log"
-                            csvfilename = file_arg_target_dir+"/testssl__"+filename+".csv"
-                            htmlfilename = file_arg_target_dir+"/testssl__"+filename+".html"
-                            jsonfilename = file_arg_target_dir+"/testssl__"+filename+".json"
+                        # specify a directory path to hold all testssl arg: --*file <filenames>
+                        file_arg_target_dir = testssl_outputdir + "/" + service_record["swarm_name"] + "/" +service_record["name"] + "/" + target_url.replace("https://","").replace(":","_")
 
-                            # append the actuall flags + file log args for the target_url
-                            testssl_sh_commands += testssl_nonfile_args + " --logfile %s --jsonfile-pretty %s --csvfile %s --htmlfile %s %s\n" % (logfilename,jsonfilename,csvfilename,htmlfilename,target_url)
+                        # filenames for all types
+                        logfilename = file_arg_target_dir+"/testssl__"+filename+".log"
+                        csvfilename = file_arg_target_dir+"/testssl__"+filename+".csv"
+                        htmlfilename = file_arg_target_dir+"/testssl__"+filename+".html"
+                        jsonfilename = file_arg_target_dir+"/testssl__"+filename+".json"
+
+                        # append the actuall flags + file log args for the target_url
+                        testssl_sh_commands += testssl_nonfile_args + " --logfile %s --jsonfile-pretty %s --csvfile %s --htmlfile %s %s\n" % (logfilename,jsonfilename,csvfilename,htmlfilename,target_url)
 
         # write it out...
         if output_filename is not None:
@@ -105,12 +139,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input-filename', dest='input_filename', default="servicechecksdb.json", help="Filename of layer check check database")
     parser.add_argument('-o', '--output-filename', dest='output_filename', default="testssl_input.txt")
-    parser.add_argument('-a', '--testssl-nonfile-args', dest='testssl_nonfile_args', help='default "-S -P -p --fast"', default="-S -P -p --fast")
+    parser.add_argument('-a', '--testssl-nonfile-args', dest='testssl_nonfile_args', help='any valid testssh.sh argument other than any of the "--*file" destination arguments, default "-S -P -p --fast"', default="-S -P -p --fast")
     parser.add_argument('-d', '--testssl-outputdir', dest='testssl_outputdir', help='for each command generated, the root output dir for all --*file arguments, default "testssl_output"', default="testssl_output")
     parser.add_argument('-x', '--log-level', dest='log_level', default="DEBUG", help="log level, default DEBUG ")
     parser.add_argument('-b', '--log-file', dest='log_file', default=None, help="Path to log file, default None, STDOUT")
     parser.add_argument('-z', '--stdout-result', action='store_true', help="print results to STDOUT in addition to output-filename on disk")
     parser.add_argument('-e', '--fqdn-filter', dest='fqdn_filter', default=None, help="Regex filter to limit which FQDNs actually included in output")
+    parser.add_argument('-B', '--uri-bucket-filter', dest='uri_bucket_filter', default=None, help="Regex filter to limit which 'unique_entrypoint_uris.[bucketname]' to actually included in output")
+    parser.add_argument('-c', '--collapse-on-fqdn-filter', dest='collapse_on_fqdn_filter', default=None, help="Capturing Regex filter to match on fqdns that share a common element and limit the test to only one of those matches, the first one found. For wildcard certs, this might be something like '.*(.wildcard.domain)'")
 
     args = parser.parse_args()
 
@@ -119,4 +155,4 @@ if __name__ == '__main__':
                         filename=args.log_file,filemode='w')
     logging.Formatter.converter = time.gmtime
 
-    execute(args.input_filename,args.output_filename,args.stdout_result,args.fqdn_filter,args.testssl_nonfile_args,args.testssl_outputdir)
+    execute(args.input_filename,args.output_filename,args.stdout_result,args.fqdn_filter,args.testssl_nonfile_args,args.testssl_outputdir,args.uri_bucket_filter,args.collapse_on_fqdn_filter)
